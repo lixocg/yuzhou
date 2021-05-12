cc4e4ee4eae5952547dc3f168a5fb2e9ae33ad5c
yuzhou <709597311@qq.com>
2021-05-12 21:49:28 +0800

    demo

---
+++ yuzhou-demo/src/main/java/com/yuzhou/demo/App.java
@@ -0,0 +1,13 @@
+package com.yuzhou.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
